5d85396ad8a31d82425d10a5b0e66cd54cc63855
Coordinate35 <Coordinate35@hust.edu.cn>
2017-03-06 12:42:46 +0800

    update quicksort

--- data_structure_and_algorithm/quick_sort.c
+++ data_structure_and_algorithm/quick_sort.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void quick_sort(int *a, int l, int r) {
+void quicksort(int *a, int l, int r) {
     int i, j, temp;
-    int pivot = a[l];
+    int privot = a[l];
     j = l;
-    if (l < r) {
-        for (i = l; i <= r; i++) {
-            if (a[i] < pivot) {
-                temp = a[j];
-                a[j] = a[i];
-                a[i] = temp;
-                j++;
-            } 
+    for (i = l; i <= r; i++) {
+        if (a[i] < privot) {
+            temp = a[j];
+            a[j] = a[i];
+            a[i] = a[j + 1];
+            a[j + 1] = temp;
+            j++;
         }
-        quick_sort(a, l, j - 1);
-        quick_sort(a, j + 1, r);
+        quicksort(a, l, j - 1);
+        quicksort(a, j + 1, r);
     }
 }
 
